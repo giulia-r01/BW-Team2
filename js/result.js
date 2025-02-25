80542219dace71e6f0835e59ed4c6d2f132fc52f
Pierattilio-correale <pierattiliocorreale@yahoo.it>
2025-02-25 10:22:06 +0100

    adds doughnut

---
+++ js/result.js
@@ -0,0 +1,36 @@
+let myCanvas = document.getElementById("myCanvas").getContext("2d");
+let myLabels = ["Risposte sbagliate", "Risposte corrette"];
+let myData = [4, 6];
+let chart = new Chart(myCanvas, {
+  type: "doughnut",
+  data: {
+    labels: myLabels,
+    datasets: [
+      {
+        label: "Risposte",
+        data: myData,
+        backgroundColor: ["#C2128D", "#00FFFF"],
+      },
+    ],
+  },
+  options: {
+    rotation: Math.PI,
+    plugins: {
+      tooltip: {
+        callbacks: {
+          label: function (tooltipItem) {
+            let label = tooltipItem.label || "";
+            let dataIndex = tooltipItem.dataIndex;
+            let value = tooltipItem.raw;
+            if (dataIndex === 0) {
+              return `${label} ${value} `;
+            } else if (dataIndex === 1) {
+              return `${label} ${value} `;
+            }
+            return label;
+          },
+        },
+      },
+    },
+  },
+});
